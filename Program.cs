a26c7f3bad70aeb0e8bcfb36e0e2d778b7f8425b
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-09-22 08:38:45 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿//*****************************************************************************
+//** 440. K-th Smallest in Lexicographical Order    leetcode                 **
+//*****************************************************************************
+
+
+int countSteps(int n, long curr, long next) {
+    int steps = 0;
+    while (curr <= n) {
+        steps += (int)fmin(n + 1, next) - curr;
+        curr *= 10;
+        next *= 10;
+    }
+    return steps;
+}
+
+int findKthNumber(int n, int k) {
+    long curr = 1;
+    k--;  // We are finding the first number, so reduce k by 1.
+
+    while (k > 0) {
+        int steps = countSteps(n, curr, curr + 1);
+        if (steps <= k) {
+            // Move to the next lexicographical number.
+            curr++;
+            k -= steps;
+        } else {
+            // Move to the next level (deeper into the tree).
+            curr *= 10;
+            k--;
+        }
+    }
+
+    return (int)curr;
+}
